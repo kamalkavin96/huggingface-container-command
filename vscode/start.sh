2b88d9e789e23a122a790da8512eac1ebde351e5
kamalkavin96 <86216079+kamalkavin96@users.noreply.github.com>
2026-06-27 11:55:52 +0530

    Add startup script for Nginx and code-server

---
+++ vscode/start.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -e
+
+echo "Starting Nginx..."
+nginx
+
+echo "Starting FastTerm..."
+python /app/src/main.py &
+FASTAPI_PID=$!
+
+
+echo "Installing/Starting code-server..."
+
+# Install curl if not already installed
+if ! command -v curl >/dev/null 2>&1; then
+    apt-get update
+    apt-get install -y curl
+fi
+
+# Install code-server if missing
+if ! command -v code-server >/dev/null 2>&1; then
+    curl -fsSL https://code-server.dev | sh
+fi
+
+# Create persistent storage directories inside /data
+mkdir -p /data/code-server-home
+mkdir -p /data/workspace
+
+# Start code-server in background
+HOME=/data/code-server-home \
+PROXY_DOMAIN_PATH="/vs-code" \
+code-server \
+  --bind-addr 0.0.0.0:9001 \
+  --auth none \
+  /data/workspace &
+
+wait $FASTAPI_PID
